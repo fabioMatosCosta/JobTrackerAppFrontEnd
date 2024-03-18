390c9015c163bf756da8d1ad6d0e24c6317f558a
Fábio Costa <fabiodanielmc@gmail.com>
2024-03-18 22:14:44 +0100

    do logic to submit add job post, and start the form

--- src/scenes/widgets/AddJobPost.jsx
+++ src/scenes/widgets/AddJobPost.jsx
@@ -16,7 +16,8 @@ import { useState } from "react";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
-import { UseDispatch, useDispatch } from "react-redux";
+import { UseDispatch, useDispatch, useSelector} from "react-redux";
+import { setJobPost } from "state";
 
 const jobPostSchema = yup.object().shape({
     title: yup.string().required("required"),
@@ -24,7 +25,7 @@ const jobPostSchema = yup.object().shape({
     jobLink: yup.string().required("required"),
     company: yup.string().required("required"),
     companyWebsite: yup.string().url().required("required"),
-    dateToApply: yup.date().date.max(new Date().getMonth() + 6)
+    dateToApply: yup.date()
 });
 
 const initialValuesJobPost = {
@@ -37,12 +38,130 @@ const initialValuesJobPost = {
 };
 
 const AddJobPost = () => {
+
+    const user = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { palette } = useTheme();
     
+    const addJobPost = async (values, onSubmitProps) => {
+
+        console.log(`http://localhost:3001/posts/${user._id}`)
+
+        const savedJobPost = await fetch(
+            `http://localhost:3001/posts/${user._id}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(values),
+            }
+        )
+        const savedPost = await savedJobPost.json();
+
+        if(!savedPost.message){
+            dispatch(setJobPost({post: savedPost }));
+            onSubmitProps.resetForm();
+        }else{
+            console.log(savedPost.message)
+        }
+        
+    };
+
     return(
         <WidgetWrapper>
-            <div>
-            AddJobPost
-            </div>
+        <Formik
+            onSubmit={ addJobPost }
+            initialValues={ initialValuesJobPost }
+            validationSchema={ jobPostSchema }
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Box
+                        display="grid"
+                        gap="30px"
+                        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    >
+                                <TextField
+                                    label="Title"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.firstName}
+                                    name="title"
+                                    error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                                    helperText={touched.firstName && errors.firstName}
+                                    sx={{ gridColumn: "span 2"}}
+                                />
+                                <TextField
+                                    label="Last Name"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.lastName}
+                                    name="lastName"
+                                    error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                                    helperText={touched.lastName && errors.lastName}
+                                    sx={{ gridColumn: "span 2"}}
+                                />
+                        <TextField
+                            label="Email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={Boolean(touched.email) && Boolean(errors.email)}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4"}}
+                        />
+                        <TextField
+                            label="Email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={Boolean(touched.email) && Boolean(errors.email)}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4"}}
+                        />
+                    </Box>
+
+                    {/* Message of error box */}
+                    <Box>
+                        <Typography variant="p" color="error">
+                            error
+                        </Typography>
+                    </Box>
+
+                    {/* Buttons */}
+                    <Box>
+                        <Button
+                            fullWidth
+                            type="submit"
+                            sx={{
+                                m: "2rem 0",
+                                p: "1rem",
+                                backgroundColor: palette.primary.main,
+                                color: palette.primary.dark,
+                                "&:hover": { color: palette.primary.main },
+                            }}
+                        >
+                        add
+                        </Button>
+                    </Box>
+                </form>
+            )}
+        </Formik>
         </WidgetWrapper>
     )
 };
--- src/state/index.js
+++ src/state/index.js
@@ -23,7 +23,7 @@ export const authSlice = createSlice({
             state.frontendUser = null;
             state.token = null;
         },
-        setJobPosts: (state, action) => {
+        setJobPostList: (state, action) => {
             state.jobPosts = action.payload.jobPosts;
         },
         setJobPost: (state, action) => {
